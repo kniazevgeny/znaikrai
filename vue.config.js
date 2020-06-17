5cc4f77ac1dca69b81365ec1c0ed4d15f5703f71
Evgeny Kniazev <kniazev-evg@ya.ru>
2020-06-17 11:45:07 +0300

    feat: spa prerender added

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,17 @@
+const path = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
 module.exports = {
   devServer: {
     disableHostCheck: true
   },
-  assetsDir: 'static/'
+  assetsDir: 'static/',
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, 'dist'),
+        // Required - Routes to render.
+        routes: [ '/', '/about', '/stories', '/analytics' ],
+      })
+    ]
+  }
 }
